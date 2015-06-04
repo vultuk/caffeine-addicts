2b3a1c455bdb1bb9eff36398f2e21ad6f61bf325
Simon Skinner <vultuk@localhost.localdomain>
2015-06-04 19:38:30 +0100

    Pull of all branches

---
+++ fullpull.sh
@@ -0,0 +1,1 @@
+git submodule update --init --recursive; git submodule foreach --recursive git fetch; git submodule foreach git merge origin master; git add -A; git commit -am "Pull of all branches"; git push
